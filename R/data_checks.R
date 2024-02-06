0ed48961ced8b47c72aa2594f9389077a93623ee
jameshawes <james.hawes@thevalueengineers.com>
2024-02-06 13:58:35 +0000

    check for unique ID [sc-1267]

--- R/data_checks.R
+++ R/data_checks.R
@@ -3,7 +3,8 @@
 #' Performs standard data checks on survey data. Doesn't alter the data in any
 #' way, just reports on potential data quality concerns.
 run_data_checks <- function(data,
-                            vars = NULL) {
+                            vars = NULL,
+                            quiet = FALSE) {
 
   # input validation ----
   assertthat::assert_that(
@@ -12,14 +13,39 @@ run_data_checks <- function(data,
   )
   if(!is.null(vars)) {
     assertthat::assert_that(
-    is.character(vars),
-    msg = "vars must be a character string"
+      is.character(vars),
+      msg = "vars must be a character string"
     )
   }
 
   # if no vars provided then use all ----
-  if(is.null(vars)) {vars = names(data)}
+  if(is.null(vars)) {vars <- names(data)}
+  # names(vars) <- vars
 
   # check for a unique ID ----
+  if(!quiet) message("Checking for unique ID")
+  id_check <- purrr::map_lgl(vars, ~length(unique(data[[.x]])) == nrow(data))
+  if(sum(id_check) == 0) {
+    if(!quiet) message("No unique ID")
+    unique_id <- "No ID"
+  } else if(sum(id_check) == 1) {
+    if(!quiet) (glue::glue("ID variable found: {vars[id_check]}"))
+    unique_id <- vars[id_check]
+  } else {
+    if(!quiet) {
+      print(
+        glue::glue(
+          "Multiple potential ID variables found: {stringr::str_flatten_comma(vars[id_check])}"
+        )
+      )
+    }
+    unique_id <- vars[id_check]
+  }
 
+  # return check results ----
+  return(
+    list(
+      unique_id = unique_id
+    )
+  )
 }
--- tests/testthat/test-data_checks.R
+++ tests/testthat/test-data_checks.R
@@ -3,10 +3,25 @@ test_that("input validation works", {
   expect_error(run_data_checks(1:3))
   expect_error(run_data_checks(letters[1:3]))
   expect_error(run_data_checks(list(letters[1:3])))
-  expect_no_error(run_data_checks(test_dat))
+  expect_no_error(run_data_checks(test_dat, quiet = TRUE))
   # vars should be a character vector
   expect_error(run_data_checks(test_dat, tibble(a = 1)))
   expect_error(run_data_checks(test_dat, 1:3))
   expect_error(run_data_checks(test_dat, list(letters[1:3])))
-  expect_no_error(run_data_checks(test_dat, c("q1", "q1.1")))
+  expect_no_error(run_data_checks(test_dat, c("q1", "q1.1"), quiet = TRUE))
+})
+
+test_that("unique IDs checks", {
+  # should find respid in test that
+  expect_equal(run_data_checks(test_dat, quiet = TRUE)$unique_id, "respid")
+  # should find no ID when respid removed
+  expect_equal(run_data_checks(test_dat[-1], quiet = TRUE)$unique_id, "No ID")
+  # should list multiple ids when more than 1 unique var
+  expect_equal(
+    run_data_checks(
+      tibble::add_column(test_dat, respid2 = nrow(test_dat):1),
+      quiet = TRUE
+    )$unique_id,
+    c("respid", "respid2")
+  )
 })
