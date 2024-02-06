cac20f1bb7decb51e6137c5a81bc899f1b48b144
jameshawes <james.hawes@thevalueengineers.com>
2024-02-06 14:03:33 +0000

    updated documentation [sc-1267]

--- R/data_checks.R
+++ R/data_checks.R
@@ -2,6 +2,13 @@
 #'
 #' Performs standard data checks on survey data. Doesn't alter the data in any
 #' way, just reports on potential data quality concerns.
+#'
+#' @param data Data frame to be tested.
+#' @param vars Optional character vector of variable names to be tested within
+#'   data
+#' @param quiet Whether to display messages in the console. Default is true.
+#'
+#' @export
 run_data_checks <- function(data,
                             vars = NULL,
                             quiet = FALSE) {
