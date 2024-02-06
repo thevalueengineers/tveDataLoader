d911353da4984d7d2ef797343980d46780f14323
jameshawes <james.hawes@thevalueengineers.com>
2024-02-06 13:16:44 +0000

    tests on vars [sc-1267]

--- tests/testthat/test-data_checks.R
+++ tests/testthat/test-data_checks.R
@@ -1,5 +1,12 @@
 test_that("input validation works", {
+  # data should be a data frame
   expect_error(run_data_checks(1:3))
   expect_error(run_data_checks(letters[1:3]))
   expect_error(run_data_checks(list(letters[1:3])))
+  expect_no_error(run_data_checks(test_dat))
+  # vars should be a character vector
+  expect_error(run_data_checks(test_dat, tibble(a = 1)))
+  expect_error(run_data_checks(test_dat, 1:3))
+  expect_error(run_data_checks(test_dat, list(letters[1:3])))
+  expect_no_error(run_data_checks(test_dat, c("q1", "q1.1")))
 })
